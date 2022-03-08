0d6d9c674a8c2b2b1d11711271539cd324bd55b0
boocipher <boocipher@gmail.com>
2022-03-08 00:15:42 -0600

    Creates structure and index.js at root

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+// GET USERS - displays all Users
+// GET Thoughts - displays all Users
+// POST users - creates user
+// PUT users - updates user
+// DELETE users - deletes
+// POST thoughts - creates
+// PUT thoughts - updates
+// DELETE thoughts - deletes
+// POST reaction - creates reaction for thought
+// DELETE reaction - deletes reaction for thought
+// POST friend - creates friend for user
+// DELETE friend - deletes friend for user
