217e66ea13c82c3a48f0775c4e80dc76e9c74079
boocipher <boocipher@gmail.com>
2022-03-08 23:56:02 -0600

    Adds thoughts.js

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,24 @@
-// GET USERS - displays all Users
-// GET Thoughts - displays all Thoughts
+// GET USERS - displays all Users: model Users, controller users; 
 // POST users - creates user
 // PUT users - updates user
 // DELETE users - deletes
+
+// GET Thoughts - displays all Thoughts : model Thoughts, controller thoughts; 
 // POST thoughts - creates
 // PUT thoughts - updates
 // DELETE thoughts - deletes
-// POST reaction - creates reaction for thought
+
+// POST reaction (in thoughts)- creates reaction for thought
 // DELETE reaction - deletes reaction for thought
-// POST friend - creates friend for user
+
+// POST friend (in users) - creates friend for user
 // DELETE friend - deletes friend for user
+
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => res.status(404).send('That route does not exist'))
+
+module.exports = router 
