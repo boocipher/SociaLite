158a546410762e72fcdaceca48d35adc3372d716
boocipher <boocipher@gmail.com>
2022-03-09 18:28:30 -0600

    Adds users model and controller

---
+++ models/Thought.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+const moment = require('moment');
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+            get: timeStamp => moment(timeStamp).format('MMM d, YYYY')
+
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        // reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false,
+    }
+)
+
+const Thought = model('Thought', thoughtSchema, 'Thought');
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require('mongoose');
+const thoughtSchema = require('./Thought');
+
+// Schema to create Student model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trimmed: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique:true,
+      trimmed: true,
+      validate: /^([a-zA-Z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+    },
+    
+    assignments: [thoughtSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const Student = model('user', userSchema);
+
+module.exports = User;
